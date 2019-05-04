4357f0a4dca36757153281ef789566a5902cb62e
nope <nope>
2019-05-04 15:13:51 +0100

    moving it all around and biting the bullet

--- server/boxconfig.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "deb http://archive.canonical.com/ trusty partner" | sudo tee -a /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get -y install git ruby ruby-dev build-essential
-sudo gem install bundler
-cd /vagrant
-bundle install
-
