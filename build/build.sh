24b1df4aad19f1f068ce511413ecd719805796da
coldclimate <>
2022-04-02 14:14:02 +0100

    no longer needed see ./github/workflows/

--- build/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-./hugo  --cleanDestinationDir  --gc
-vendor/htmltest/htmltest ./docs/ -c ./.htmltest
--- build/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-curl -LO https://github.com/gohugoio/hugo/releases/download/v0.55.1/hugo_0.55.1_Linux-64bit.tar.gz
-tar -vzxf hugo_0.55.1_Linux-64bit.tar.gz
-chmod a+x hugo
